b2decb5637e795df333ef0f4a4b0c3d24fe8ef6e
James Kwok <jameskwok@L-C02V81BCHV2J-James-Kwok.local>
2017-10-27 14:15:31 +1100

    up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,16 @@
       <p v-if="show" style="font-size:300%;">hello</p>
     </transition>
   </div>
+  <div id="f1_container">
+    <div id="f1_card" class="shadow">
+      <div class="front face">
+        <img src="/images/Cirques.jpg"/>
+      </div>
+      <div class="back face">
+        <img src="/images/Cirques.jpg"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -46,5 +56,40 @@ a {
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0
+}
+#f1_container {
+  position: relative;
+  margin: 10px auto;
+  width: 450px;
+  height: 281px;
+  z-index: 1;
+}
+#f1_container {
+  perspective: 1000;
+}
+#f1_card {
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+  transition: all 1.0s linear;
+}
+#f1_container:hover #f1_card {
+  transform: rotateY(180deg);
+  box-shadow: -5px 5px 5px #aaa;
+}
+.face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+.face.back {
+  display: block;
+  transform: rotateY(180deg);
+  box-sizing: border-box;
+  padding: 10px;
+  color: white;
+  text-align: center;
+  background-color: #aaa;
 }
 </style>
